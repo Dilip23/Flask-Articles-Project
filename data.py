5ad9945ef44562651efefaf052e5c5e30c0906c0
test254 <onlineteach23@gmail.com>
2020-04-25 10:39:25 +0530

    Full Project

---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'title':'Article 1',
+            'body':'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'author':'Francis Delora',
+            'create_date':'05-08-1967'
+        },
+        {
+            'id':2,
+            'title':'Article 2',
+            'body':'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'author':'John Paul',
+            'create_date':'05-08-1977'
+        },
+        {
+            'id':3,
+            'title':'Article 3',
+            'body':'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+            'author':'Chris Gart',
+            'create_date':'05-11-1997'
+        }
+
+    ]
+    return articles
